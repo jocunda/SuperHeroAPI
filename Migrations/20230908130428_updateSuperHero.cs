64a64db02ce2664e45582537698cd679ad61c117
jocunda <silviawanshan88@gmail.com>
2023-09-11 14:37:24 +0800

    Second Commit

---
+++ Migrations/20230908130428_updateSuperHero.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperHeroAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateSuperHero : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Place2",
+                table: "SuperHeroes",
+                newName: "Place");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Place",
+                table: "SuperHeroes",
+                newName: "Place2");
+        }
+    }
+}
